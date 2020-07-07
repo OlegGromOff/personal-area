0ead40ace3d8d9b9f5bbf791e95b5276b015042a
OlegGromOff <rekops@ukr.net>
2020-07-07 22:27:04 +0300

    fix-links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -11,9 +11,9 @@ export default class Sidebar extends React.Component {
       <div className="sidebar-wrapper">
         <ul className="sidebar">
           <li><span><b>Мой кабинет</b></span></li>
-          <li><NavLink to="/">Личные данные</NavLink></li>
-          <li><NavLink to="/basket">Корзина</NavLink></li>
-          <li><NavLink to="/order">Мои заказы</NavLink></li>
+          <li><NavLink to="/" className="link">Личные данные</NavLink></li>
+          <li><NavLink to="/basket" className="link">Корзина</NavLink></li>
+          <li><NavLink to="/order" className="link">Мои заказы</NavLink></li>
         </ul>
       </div>
     )
